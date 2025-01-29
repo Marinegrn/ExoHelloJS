6ea7192cf982650db2f7f87d3b08b18249058c83
Marinegrn <marinegarein@outlook.fr>
2025-01-29 14:14:29 +0100

    Ajout condition et 2eme paramètre

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 
 
-function sayHello (firstName) { // Déclaration fonction
-    let message = "Bonjour "; // Décla variable message
-    message += firstName // ajout du paramètre firstname à la variable message
-    console.log(message + ' !')
+function sayHello (firstName, hour) { // Déclaration fonction et du 2ème paramètre
+    let message = 'Bonjour '// Décla' variable message
+    message += firstName; // ajout du paramètre firstName à message
+
+    // Ajout de la condition 
+    if (hour >= 18) {
+        console.log('Bonsoir ' + firstName + '!');         
+    } else { 
+        console.log(message + '!'); // // Sinon, afficher 'Bonjour Beyonce !'
+    }
 
 }
 
-sayHello("Beyonce"); // Appel de la fonction et définition du paramètre FirstName
+sayHello("Beyonce", 19); // Appel de la fonction et définition du paramètre FirstName
