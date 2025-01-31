01796d6563fb94154e38eca97a276227b4397546
Marinegrn <marinegarein@outlook.fr>
2025-01-31 10:23:40 +0100

    finalisation Exo

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// Déclaration des 2 variables
+let message = 'Bonjour ';
+let firstName = 'Beyonce';
 
+message += firstName + ' !'; // Concaténation de message et First Name
+console.log(message);
 
 
 function sayHello (firstName, hour) { // Déclaration fonction et du 2ème paramètre
     let message = 'Bonjour '// Décla' variable message
-    message += firstName; // ajout du paramètre firstName à message
+    message += firstName; // concaténation du paramètre firstName et message
 
     // Ajout de la condition 
     if (hour >= 18) {
